17b2b3b0466fe77238c6de03a14605b49e56ea33
Ekaterina Panasiuk <ekaterinapanasiuk29@gmail.com>
2023-10-29 19:57:58 +0300

    feature:props-typescr Sun Oct 29 2023 19:57:

---
+++ src/components/1app/App.tsx
@@ -0,0 +1,22 @@
+import React, { ReactElement, useState } from 'react'
+import Header from '../3widgets/Header/Header'
+import MainComponent from '../3widgets/Main/MainComponent'
+import "./style.scss"
+ function App():React.FC {
+   const [value, setValue]= useState<string>("")
+    console.log(value)
+    function handeValue(event :React.ChangeEventHandler<HTMLInputElement> ):void{
+        setValue(event.target.value)
+    }
+
+  return (<>
+    <header className='header'>
+        <h2>Hello</h2>
+        <input className='header__input' type="text" value={value} onChange={handeValue}/>
+    </header>
+    <MainComponent request={value}/>
+    </>
+  )
+}
+
+export default App
